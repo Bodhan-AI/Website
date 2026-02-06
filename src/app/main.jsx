3215794fee549eca59cdfcb6c16a5ae7316611bc
sumitbhukal0007 <sumitbhukal007@gmail.com>
2026-02-06 20:44:10 +0530

    reverted main.jsx

--- src/app/main.jsx
+++ src/app/main.jsx
@@ -6,7 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.MODE === 'production' ? '/' : '/'}>
+    <BrowserRouter basename={import.meta.env.MODE === 'production' ? '/Website' : '/'}>
       <App />
     </BrowserRouter>
   </StrictMode>,
